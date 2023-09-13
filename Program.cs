22216c0813e847ff445022c7f10a8c8be4f7468c
uhuddurmus <uhuddurmus@gmail.com>
2023-09-13 15:38:32 +0300

    Instance, Field, Property
    Instance, Field, Property

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace class_instance_field_property;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        //Access Modifiers
+        // * Public
+        // * Private
+        // * Internal
+        // * Protected
+
+
+
+        Employee employeeOne = new Employee();
+        employeeOne.Name = "Mustafa Uhud";
+        employeeOne.LastName = "Durmus";
+        employeeOne.No = 111111;
+        employeeOne.Department = "Galaktik Lord";
+
+        employeeOne.GetEmployeeInfo();
+
+        Console.WriteLine("---------------------------");
+
+        Employee employeeTwo = new Employee();
+        employeeTwo.Name = "Haydar";
+        employeeTwo.LastName = "Haydaroğlu";
+        employeeTwo.No = 222222;
+        employeeTwo.Department = "Galaktik Lord Yardımcısı";
+
+        employeeTwo.GetEmployeeInfo();
+
+    }
+
+    class Employee
+    {
+        public string Name;
+        public string LastName;
+        public int No;
+        public string Department;
+
+        public void GetEmployeeInfo()
+        {
+            Console.WriteLine("Name of the employee: {0}", Name);
+            Console.WriteLine("Last Name of the employee: {0}", LastName);
+            Console.WriteLine("Id of the employee: {0}", No);
+            Console.WriteLine("Department of the employee: {0}", Department);
+        }
+    }
+}
